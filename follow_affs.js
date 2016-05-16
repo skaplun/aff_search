603b5bee122334776183dd4bbba5bc3441738681
skaplun <shai.kaplun@gmail.com>
2016-05-16 13:14:18 +0300

    finally fixing to save aff

--- follow_affs.js
+++ follow_affs.js
@@ -2,49 +2,67 @@
 
 var User            = require('./app/models/user');
 var urlParser = require('url');
+var Promise = require('bluebird');
 
-
-function follow(sites_array, user, res){
+function follow(sites_array, user){
     
-    sites_array.forEach(function(siteObj){
+    return new Promise(function(resolve, reject){
         
-        var k = Object.keys(siteObj)[0];
-        var newK = urlParser.parse(k).hostname
-        newK = newK.replace(/\./g, '[dot]');
-        var v = siteObj[k];
-        delete siteObj[k];
-        
-        siteObj[newK] = v
-        
-        User.findOne(user, function (err, u) {
+        sites_array.forEach(function(siteObj){
+
+            var k = Object.keys(siteObj)[0];
             
-            if(u['followingAffs']['sites'].length === 0) {
-                
-                u['followingAffs']['sites'].push(siteObj);
-        
-                    u['followingAffs']['sites'].save(function (err) {
-                      if (!err){
-                          console.log(u['followingAffs']['sites'])
-                          res.json('Success!');
-                      } 
-                });
-                
-            }else{
-                
-                User.findOneAndUpdate(user, { $addToSet : { location : {$each : k} } }, { upsert : true }, function (err, u) {
-                    if(!err){
-                        console.log(u['followingAffs']['sites'])
-                        res.json('success');
-                    }else{
-                        console.log(err)
-                    }
-                })
-             
-            } 
-        })
-             
+            var newK = urlParser.parse(k).hostname;
+            
+            newK = newK.replace(/\./g, '[dot]');
+            
+            var v = siteObj[k];
+            
+            delete siteObj[k];
 
-    
+            siteObj[newK] = v
+
+            User.findOne(user, function (err, u) {
+
+                if(u['followingAffs']['sites'].length === 0) {
+
+                    u['followingAffs']['sites'].push(siteObj);
+
+                        u.save(function (err) {
+                            
+                          if (!err){
+                              
+                              resolve('Success!');
+                              
+                          }else{
+                            console.log(err)  
+                            resolve("couldn't update mongo");
+
+                          } 
+                    });
+
+                }else{
+
+                    User.findOneAndUpdate(user, { $addToSet : { location : {$each : k} } }, { upsert : true }, function (err, u) {
+                        
+                        if(!err){
+                            
+                            resolve('success');
+                            
+                        }else{
+                            
+                            console.log(err)
+                            
+                            resolve("couldn't update mongo");
+                            
+                        }
+                    })
+
+                } 
+            })
+
+        })
+
     })
    
 }
@@ -52,7 +70,9 @@ function follow(sites_array, user, res){
 function follow_affs(req, res) {
 
     var sites_array = JSON.parse(Object.keys(req.body)[0]);
-    var f = follow(sites_array, req.user, res);
+    var f = follow(sites_array, req.user, res).then(function(data) {
+        res.json(data)
+    })
     
 }
 
